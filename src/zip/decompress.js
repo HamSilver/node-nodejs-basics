4a2486598d3653baf316e44499dcf46b2764368f
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-12-05 22:11:31 +0200

    feat: Add solution for zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+
+const archive = "./files/archive.gz";
+const extractedFile = "./files/fileToCompress.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = createUnzip();
+  const streamFrom = createReadStream(archive);
+  const streamTo = createWriteStream(extractedFile);
+
+  try {
+    await pipeline(streamFrom, gunzip, streamTo);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await decompress();
