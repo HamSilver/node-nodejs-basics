c7d8feb2ee9d5b8c53f86c35f0681feb213d1688
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-12-05 21:29:59 +0200

    feat: Add solution for streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "node:fs";
+import { stdin } from "process";
+
+const fileToWrite = "./files/fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(fileToWrite, { encoding: "utf8" });
+
+  stdin.on("data", (data) => {
+    stream.end(data.toString());
+    process.exit();
+  });
 };
 
 await write();
