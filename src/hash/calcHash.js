a393a8fc7a16e8576882178a971732c70b8cd880
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-12-05 20:26:45 +0200

    feat: Add solution for hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+
+const fileToRead = "./files/fileToCalculateHashFor.txt";
+const errorMsg = "File reading failed";
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await readFile(fileToRead, { encoding: "utf8" });
+    const hash = createHash("sha256");
+    console.log(hash.update(data).digest("hex"));
+  } catch (_) {
+    throw new Error(errorMsg);
+  }
 };
 
 await calculateHash();
