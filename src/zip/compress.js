980efd5033554bad51f183795cf76519b9a4c32b
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-12-05 22:01:51 +0200

    feat: Add solution for zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+
+const fileToCompress = "./files/fileToCompress.txt";
+const archive = "./files/archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const streamFrom = createReadStream(fileToCompress);
+  const streamTo = createWriteStream(archive);
+
+  try {
+    await pipeline(streamFrom, gzip, streamTo);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await compress();
