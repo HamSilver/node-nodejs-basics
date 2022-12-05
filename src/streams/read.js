39ab120aef20031ffdcd6c6394d277b4b5ca5e5d
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-12-05 21:20:26 +0200

    feat: Add solution for streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { stdout } from "process";
+import { createReadStream } from "node:fs";
+
+const fileToRead = "./files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(fileToRead, { encoding: "utf8" });
+
+  stream.on("data", (data) => {
+    stdout.write(data);
+  });
+
+  stream.on("error", (error) => {
+    throw error;
+  });
 };
 
 await read();
